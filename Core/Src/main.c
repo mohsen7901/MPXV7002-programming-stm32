506750cefe520f434fdbeb158be55bd492c31374
Mohsen Fathali <109556621+mohsen7901@users.noreply.github.com>
2024-08-26 23:35:36 +0330

    Add statistic data to project
    These statistics implemented in project :
    mean, max, and min over some data in the specified time.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+//#include "math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -35,6 +35,13 @@
 #define NUM_OF_ADC_DATA 200
 #define True 1
 #define False 0
+#define SAMPLING_FRQ 10     // 1 to 10
+#define MAX_SAMPLING_FRQ 10
+#define MAX_PRESSURE_DATA (1000/(SAMPLING_FRQ))
+//#define MEAN_PRESSURE_STEP_SIZE 1000/1
+//#define MAX_NUM_MEAN 1  
+#define TOTAL_NUM_DATA (MAX_PRESSURE_DATA*NUM_OF_ADC_DATA)
+
 
 /* USER CODE END PD */
 
@@ -48,15 +55,33 @@ ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
 TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim3;
 
 /* USER CODE BEGIN PV */
 
 uint32_t adc_data[NUM_OF_ADC_DATA];
-volatile float pressure = 0;
+volatile float data[MAX_SAMPLING_FRQ];  // number of pressure data depends on sampling frequency
 uint32_t adc_counter = 0;
 uint32_t timer_counter = 0;
 volatile uint8_t adc_ready = False;
-
+uint32_t sum_of_adc_data = 0;  // this var stores sum of the sum of the recieved data from dma
+//volatile uint16_t d_counter = 0;
+//volatile float mean_pressure = 0;
+volatile uint8_t data_ready = False;
+volatile uint8_t p_counter = 0;  // An index for pressure
+uint8_t sampling_frq = SAMPLING_FRQ; //1 to 10
+uint8_t terminate_time = 3;  // the maximum time need to wait for measuring statistics data
+uint16_t num_of_means;
+volatile float sum_of_means = 0;
+volatile float means[600]; //an array with the max number of mean data required
+uint16_t means_counter = 0;
+//float previous_data = 0;
+volatile float min_pressure;
+volatile float max_pressure;
+volatile float mean_pressure;
+volatile float std_pressure;
+volatile uint8_t timer_status = False;
+volatile uint8_t loop_counter = 0;
 
 /* USER CODE END PV */
 
@@ -66,8 +91,12 @@ static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
-void measure_pressure(void);
+uint32_t get_sum_of_data(void);
+float measure_pressure(float data);
+float meassure_mean_p(void);
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -107,11 +136,15 @@ int main(void)
   MX_DMA_Init();
   MX_ADC1_Init();
   MX_TIM1_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-	
-	
+	num_of_means = terminate_time * sampling_frq;
+
 	HAL_ADC_Start_DMA(&hadc1, adc_data, NUM_OF_ADC_DATA);
+	
+	__HAL_TIM_SetAutoreload(&htim1, 64000/sampling_frq) ;
 	HAL_TIM_Base_Start_IT(&htim1);
+	HAL_TIM_Base_Start_IT(&htim3);
 	
   /* USER CODE END 2 */
 
@@ -119,13 +152,65 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+		if(timer_status == True)
+		{
+			timer_status = False;
+			// measure std
+			means_counter = 0;
+			//sum_of_means = sum_of_means - means[means_counter] + data[p_counter];
+			
+		}
+		
+		if(data_ready == True)
+		{
+			
+			data_ready = False;
+			if(p_counter == sampling_frq)
+			{
+				p_counter =0;
+			}
+			data[p_counter] = meassure_mean_p();
+			sum_of_adc_data = 0;
+			
+			
+			sum_of_means = sum_of_means - means[means_counter] + data[p_counter];
+			if(means_counter == 0 && loop_counter == 0)
+			{
+				means[0] = 2250.01;
+				min_pressure = means[0];
+			}
+			else
+			{
+				means[means_counter] = data[p_counter];
+			}
+			mean_pressure = sum_of_means / num_of_means;
+			
+			if (min_pressure > means[means_counter])
+			{
+				min_pressure = means[means_counter];
+			}
+			if (max_pressure < means[means_counter])
+			{
+				max_pressure = means[means_counter];
+			}
+			
+			means_counter++;
+			// pressure[p_counter] = measure_pressure(mean_data);
+			p_counter++;
+			
+
+		}
+		
 		if(adc_ready == True)
 		{
 			adc_ready=False;
-			measure_pressure();
+			sum_of_adc_data += get_sum_of_data();
+			
 		}
+		
+		loop_counter++;
     /* USER CODE END WHILE */
-
+		
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -280,6 +365,51 @@ static void MX_TIM1_Init(void)
 }
 
 /**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 999;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 64000;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
   * Enable DMA controller clock
   */
 static void MX_DMA_Init(void)
@@ -330,22 +460,42 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	{
 		timer_counter = adc_counter;
 		adc_counter = 0;
-
+		data_ready = True;
+	}
+	if(htim == &htim3)
+	{
+		terminate_time--;
+		if(terminate_time == 0)
+		{
+			terminate_time = 3;
+			timer_status = True;
+			data_ready = False;
+		}
 	}
 }
 
-void measure_pressure(void)
+uint32_t get_sum_of_data(void)
 {
-		float adc_temp_data = 0;
-		for(uint8_t i = 0;i < NUM_OF_ADC_DATA;i++)
-		{
-			adc_temp_data += (float) adc_data[i];
-		}
-		
-		adc_temp_data /= NUM_OF_ADC_DATA;
-		
-		pressure = (adc_temp_data * 3.3 * 1.5 / 4095) - 2.5;
-		
+	uint32_t adc_temp_data = 0;
+	for(uint8_t i = 0;i < NUM_OF_ADC_DATA;i++)
+	{
+		adc_temp_data += adc_data[i];
+	}
+
+	//adc_temp_data /= NUM_OF_ADC_DATA;
+	return adc_temp_data;
+}
+
+float measure_pressure(float data)
+{
+	float temp_pressure = (data * 3.3 * 1.5 / 4095) - 2.5;
+	return temp_pressure;
+}
+
+float meassure_mean_p(void)
+{
+	float mean = sum_of_adc_data / (float)(TOTAL_NUM_DATA);
+	return mean;
 }
 
 /* USER CODE END 4 */
--- Core/Src/stm32g0xx_it.c
+++ Core/Src/stm32g0xx_it.c
@@ -58,6 +58,7 @@
 extern DMA_HandleTypeDef hdma_adc1;
 extern ADC_HandleTypeDef hadc1;
 extern TIM_HandleTypeDef htim1;
+extern TIM_HandleTypeDef htim3;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -182,6 +183,20 @@ void TIM1_BRK_UP_TRG_COM_IRQHandler(void)
   /* USER CODE BEGIN TIM1_BRK_UP_TRG_COM_IRQn 1 */
 
   /* USER CODE END TIM1_BRK_UP_TRG_COM_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM3 global interrupt.
+  */
+void TIM3_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM3_IRQn 0 */
+
+  /* USER CODE END TIM3_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim3);
+  /* USER CODE BEGIN TIM3_IRQn 1 */
+
+  /* USER CODE END TIM3_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
